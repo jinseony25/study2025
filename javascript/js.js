98d242e02734b225701509cb2159ea703f2879f0
jinseony25 <jinseony25@gmail.com>
2025-07-16 14:28:43 +0900

    responsive

---
+++ javascript/js.js
@@ -0,0 +1,17 @@
+// 한줄주석
+// 제이쿼리선언
+
+// 화면을 열었을때, 다음과 같은 일들이 일어나라
+
+$(document).ready(function(){
+
+// 실행문 작성
+// 클래스 토글을 클릭했을때, 클래스 gnb가 슬라이드 toggle되어라
+
+$('.toggle').click(function(){
+    
+$('.gnb').slideToggle()
+})
+
+
+})
